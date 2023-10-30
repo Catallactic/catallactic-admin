7b95d41b5f8e9b5115ace10202d7814a0ab3f835
pellyadolfo <pellyadolfo@yahoo.es>
2023-10-30 19:40:19 +0100

    upgraded libraries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 // src/pages/_app.tsx
 import type { AppProps } from 'next/app'
+
 import { AdminLayout } from 'layout'
+import { SimpleLayout } from 'layout/SimpleLayout';
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.scss'
 import '../styles/_app.css';
-import { SimpleLayout } from 'layout/SimpleLayout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
