16d2f74c4792ac0ba68e6d60377c7fa0d5ad6de6
pellyadolfo <pellyadolfo@yahoo.es>
2024-09-15 20:45:31 +0200

    updated footer

--- src/layout/AdminLayout/Footer/Footer.tsx
+++ src/layout/AdminLayout/Footer/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
     <footer className="footer border-top px-sm-2 py-2 catatext">
       <Container fluid className="align-items-center flex-column flex-md-row d-flex justify-content-between">
         <div>
-          <a className="text-decoration-none" href="https://www.catallactic.org/">Catallactic</a> {' '} © 2024 GasClick.
+          <a className="text-decoration-none" href="https://www.catallactic.org/" target="_blank">Catallactic</a> {' '} © 2024.
         </div>
         <div className="ms-md-auto">
-          Powered by&nbsp; <a className="text-decoration-none" href="https://www.catallactic.org/">CryptoCommodities</a>
+          Powered by&nbsp; <a className="text-decoration-none" href="https://www.catallactic.org/" target="_blank">CryptoCommodities</a>
         </div>
       </Container>
     </footer>
