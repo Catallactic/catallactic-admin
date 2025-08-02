034b5749be9bc120dd785a8125350bb5e4787d2a
pellyadolfo <pellyadolfo@yahoo.es>
2025-08-02 14:13:06 +0200

    udpate year

--- src/layout/AdminLayout/Footer.tsx
+++ src/layout/AdminLayout/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="footer border-top px-sm-2 py-2 catatext">
       <Container fluid className="align-items-center flex-column flex-md-row d-flex justify-content-between">
         <div>
-          <a className="text-decoration-none" href="https://www.catallactic.org/" target="_blank">Catallactic</a> {' '} © 2024.
+          <a className="text-decoration-none" href="https://www.catallactic.org/" target="_blank">Catallactic</a> {' '} © 2025.
         </div>
         <div className="ms-md-auto">
           Powered by&nbsp; <a className="text-decoration-none" href="https://www.catallactic.org/" target="_blank">CryptoCommodities</a>
