35a018dda1e68e710772d23008923769a94381fe
pellyadolfo <pellyadolfo@yahoo.es>
2025-08-06 18:46:34 +0200

    added officanvas redirections

--- src/layout/AdminLayout/OffCanvas.tsx
+++ src/layout/AdminLayout/OffCanvas.tsx
@@ -1,5 +1,6 @@
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { redirect } from 'next/navigation';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
@@ -23,51 +24,51 @@ function OffCanvas() {
         <Offcanvas.Body>
 
 					<p className="fs-4">Create CryptoCommodity</p>
-					<p className="mb-0">ISSUER: Create Cryptocommodity</p>
-          <p className="mb-0">ISSUER: Select Cryptocommodity</p>
-          <p className="mb-0">ISSUER: Install Facets</p>
-          <p className="mb-0">ISSUER: Create Storage</p>
-          <p className="mb-0">ISSUER: Initialize Cryptocommodity ERC-20 with Name, Symbol, Cryptocommodity Supply</p>
+					<p className="mb-0" onClick={() => (redirect('/admin/cryptocommodities'))} >ISSUER: Create Cryptocommodity</p>
+          <p className="mb-0" onClick={() => (redirect('/admin/cryptocommodities'))} >ISSUER: Select Cryptocommodity</p>
+          <p className="mb-0" onClick={() => (redirect('/admin/cryptocommodities'))} >ISSUER: Install Facets</p>
+          <p className="mb-0" onClick={() => (redirect('/admin/cryptocommodities'))} >ISSUER: Create Storage</p>
+          <p className="mb-0" onClick={() => (redirect('/erc20/features'))} >ISSUER: Initialize Cryptocommodity ERC-20 with Name, Symbol, Cryptocommodity Supply</p>
 					<br/>
 
 					<p className="fs-4">Create Funding Round</p>
-					<p className="mb-0">ISSUER: Create Vesting Period</p>
-          <p className="mb-0">ISSUER: Create Funding Round</p>
-          <p className="mb-0">ISSUER: Configure Antiwhale</p>
-          <p className="mb-0">ISSUER: Install Payment Methods</p>
-          <p className="mb-0">ISSUER: Set the setReceiveFacet (the Crowdsale facet address)</p>
+					<p className="mb-0" onClick={() => (redirect('/vesting/features'))} >ISSUER: Create Vesting Period</p>
+          <p className="mb-0" onClick={() => (redirect('/rounds/features'))} >ISSUER: Create Funding Round</p>
+          <p className="mb-0" onClick={() => (redirect('/rounds/antiwhale'))} >ISSUER: Configure Antiwhale</p>
+          <p className="mb-0" onClick={() => (redirect('/rounds/payments'))} >ISSUER: Install Payment Methods</p>
+          <p className="mb-0" onClick={() => (redirect('/rounds/payments'))} >ISSUER: Set the setReceiveFacet (the Crowdsale facet address)</p>
 					<br/>
 
 					<p className="fs-4">Manage Funding Round</p>
-					<p className="mb-0">ISSUER: Start Funding Round</p>
-          <p className="mb-0">INVESTOR: Find Cryptocommodity</p>
-          <p className="mb-0">INVESTOR: Invest Token</p>
-          <p className="mb-0">INVESTOR: Invest Coin</p>
-          <p className="mb-0">ISSUER: Whitelist Investor</p>
-          <p className="mb-0">INVESTOR: Invest Token</p>
-          <p className="mb-0">INVESTOR: Invest Coin</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Start Funding Round</p>
+          <p className="mb-0" onClick={() => (redirect('/admin/cryptocommodities'))} >INVESTOR: Find Cryptocommodity</p>
+          <p className="mb-0" onClick={() => (redirect('/admin/accounts'))} >INVESTOR: Invest Token</p>
+          <p className="mb-0" onClick={() => (redirect('/admin/accounts'))} >INVESTOR: Invest Coin</p>
+          <p className="mb-0" onClick={() => (redirect('/rounds/antiwhale'))} >ISSUER: Whitelist Investor</p>
+          <p className="mb-0" onClick={() => (redirect('/admin/accounts'))} >INVESTOR: Invest Token</p>
+          <p className="mb-0" onClick={() => (redirect('/admin/accounts'))} >INVESTOR: Invest Coin</p>
 					<br/>
 
 					<p className="fs-4">Finalize Funding Round with Failure</p>
-					<p className="mb-0">ISSUER: Finalize Funding Round</p>
-					<p className="mb-0">INVESTOR: Refund invested tokens</p>
-					<p className="mb-0">INVESTOR: Refund invested coins</p>
-					<p className="mb-0">ISSUER: Reset Funding Round</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Finalize Funding Round</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >INVESTOR: Refund invested tokens</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >INVESTOR: Refund invested coins</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Reset Funding Round</p>
 					<br/>
 
 					<p className="fs-4">Finalize Funding Round with Success</p>
-					<p className="mb-0">ISSUER: Finalize Funding Round</p>
-					<p className="mb-0">ISSUER: Enter Vesting Address in Crowdsale</p>
-					<p className="mb-0">ISSUER: Enter Token Address in Crowdsale</p>
-					<p className="mb-0">ISSUER: Enter Token Address in Vesting</p>
-					<p className="mb-0">ISSUER: Enter Crowdsale Address as Vesting Grantor</p>
-					<p className="mb-0">ISSUER: Transfers to Crowdsale Total Cryptocommodities purchased</p>
-					<p className="mb-0">INVESTOR: Claims purchased Cryptocommodities</p>
-					<p className="mb-0">INVESTOR: Waits Vesting Periods</p>
-					<p className="mb-0">INVESTOR: Release Vesting Slides</p>
-					<p className="mb-0">ISSUER: Configures Withdraw Wallet</p>
-					<p className="mb-0">ISSUER: Withdraws Funds to Wallet</p>
-					<p className="mb-0">ISSUER: Runs TGE to Exchanges (Optionally)</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Finalize Funding Round</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Enter Vesting Address in Crowdsale</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Enter Token Address in Crowdsale</p>
+					<p className="mb-0" onClick={() => (redirect('/vesting/operations'))} >ISSUER: Enter Token Address in Vesting</p>
+					<p className="mb-0" onClick={() => (redirect('/vesting/operations'))} >ISSUER: Enter Crowdsale Address as Vesting Grantor</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Transfers to Crowdsale Total Cryptocommodities purchased</p>
+					<p className="mb-0" onClick={() => (redirect('/admin/accounts'))} >INVESTOR: Claims purchased Cryptocommodities</p>
+					<p className="mb-0" onClick={() => (redirect('/vesting/operations'))} >INVESTOR: Waits Vesting Periods</p>
+					<p className="mb-0" onClick={() => (redirect('/vesting/operations'))} >INVESTOR: Release Vesting Slides</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Configures Withdraw Wallet</p>
+					<p className="mb-0" onClick={() => (redirect('/rounds/operations'))} >ISSUER: Withdraws Funds to Wallet</p>
+					<p className="mb-0" onClick={() => (redirect('/admin/contract'))} >ISSUER: Runs TGE to Exchanges (Optionally)</p>
 					<br/>
 
         </Offcanvas.Body>
