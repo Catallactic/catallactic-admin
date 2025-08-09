986db4e9a98944ce37d52b6fb41a05782525d8da
pellyadolfo <pellyadolfo@yahoo.es>
2025-08-09 20:34:35 +0200

    updated button styles

--- src/layout/AdminLayout/Header.tsx
+++ src/layout/AdminLayout/Header.tsx
@@ -180,7 +180,7 @@ export default function Header(props: HeaderProps) {
 					{wallet && CRYPTOCOMMODITIES && CRYPTOCOMMODITIES.length > 0 ?
 						<Dropdown className="btn mx-2 my-0 dropdown p-0 border-0" onSelect={onSelectCryptocommodity} >
 
-							<Dropdown.Toggle className="w-100 bg-header border-0" disabled={!CRYPTOCOMMODITIES || CRYPTOCOMMODITIES.length == 0}>
+							<Dropdown.Toggle className="w-100 p-2 bg-connected fw-bolder border-0" disabled={!CRYPTOCOMMODITIES || CRYPTOCOMMODITIES.length == 0}>
 								{ selectedCrypto?.SELECTED_CRYPTOCOMMODITY_NAME ? <span className='text-white mx-2 text-decoration-none'> {selectedCrypto?.SELECTED_CRYPTOCOMMODITY_NAME} </span>: <FontAwesomeIcon size="xl" icon={faCoins} className='text-white mx-2' /> }
 							</Dropdown.Toggle>
 
